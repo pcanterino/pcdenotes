80a429d30dd44b0274bf92486fa7639441116ced
Patrick Canterino <patrick@patrick-canterino.de>
2022-01-23 18:26:22 +0100

    Changed the way the .env file is loaded

--- pcdenotes/settings.py
+++ pcdenotes/settings.py
@@ -13,13 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
 
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env file
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
