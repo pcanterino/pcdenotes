f99300eecfd92abe9b7b442456694950fb3c54fa
Patrick Canterino <patrick@patrick-canterino.de>
2022-01-23 22:02:33 +0100

    Do pagination only if there is more than one page

--- notes/views.py
+++ notes/views.py
@@ -17,10 +17,12 @@ def note_list(request):
         page_number = int(request.GET.get('page'))
     except:
         pass
+
+    page_count = paginator.num_pages
     
     notes_page = paginator.get_page(page_number)
 
-    return render(request, 'note_list.html', {'notes_page': notes_page, 'notes_count': notes_count, 'pages': paginator, 'page_number': page_number})
+    return render(request, 'note_list.html', {'notes_page': notes_page, 'notes_count': notes_count, 'pages': paginator, 'page_number': page_number, 'page_count': page_count})
 
 def note_detail(request, note_slug):
     note = get_object_or_404(Note, slug=note_slug, status=1)
