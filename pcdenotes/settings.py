f534e3e94d119d6baa2edebfd69e3d1ee055e57b
Patrick Canterino <patrick@patrick-canterino.de>
2022-01-30 18:47:36 +0100

    Moved some styles into a CSS file

--- pcdenotes/settings.py
+++ pcdenotes/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [BASE_DIR / 'notes/static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
